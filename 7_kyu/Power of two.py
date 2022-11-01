ed519855232ad80e31cd7d28c977d67a02b4e813
AlexandrMolyavin <117174004+AlexandrMolyavin@users.noreply.github.com>
2022-11-01 16:43:28 +0300

    Update Power of two.py

--- 7_kyu/Power of two.py
+++ 7_kyu/Power of two.py
@@ -1,12 +1,10 @@
-def check_exam(arr1,arr2):
-    score = 0
-    for i1,item1 in enumerate(arr1):
-        for i2,item2 in enumerate(arr2):
-            if i1 == i2 :
-                if item1 == item2:
-                    score+=4
-                    break
-                if (item2 != item1) and (item2 != ""):
-                    score-=1
-                    break
-    return score if score>0 else 0
+def power_of_two(x):
+    i = 2
+    if x==2 or x==1:
+        return True
+    if x%2!=0:
+         return False
+    while i<x:
+        i=i*2
+    return i == x 
+  # your code here
