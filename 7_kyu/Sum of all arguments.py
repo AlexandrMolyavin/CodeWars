8c821b723bd5e4a0a294ce4723c25c17490b434a
AlexandrMolyavin <117174004+AlexandrMolyavin@users.noreply.github.com>
2022-11-01 16:43:05 +0300

    Update Sum of all arguments.py

--- 7_kyu/Sum of all arguments.py
+++ 7_kyu/Sum of all arguments.py
@@ -1,10 +1,6 @@
-def power_of_two(x):
-    i = 2
-    if x==2 or x==1:
-        return True
-    if x%2!=0:
-         return False
-    while i<x:
-        i=i*2
-    return i == x
-  # your code here
+def sum_args(*x):
+    # ...
+    a = 0
+    for item in x:
+        a+=item
+    return a
