82e7cd0bb1730f57d3efae686902f27941afdfe0
AlexandrMolyavin <117174004+AlexandrMolyavin@users.noreply.github.com>
2022-11-01 16:45:26 +0300

    Update The Office II - Boredom Score.py

--- 7_kyu/The Office II - Boredom Score.py
+++ 7_kyu/The Office II - Boredom Score.py
@@ -1,8 +1,22 @@
-def search_names(logins):
-    pass
-    a = []
-    for item1 in logins:
-        for item2 in item1:
-            if item2[len(item2)-1]=='_':
-                a.append(item1)
-    return a
+def boredom(staff):
+    #your code here
+    sum = 0
+    b = {"accounts" :1,
+        "finance" : 2,
+        "canteen" : 10,
+        "regulation" : 3,
+        "trading" : 6,
+        "change" : 6,
+        "IS" : 8,
+        "retail" : 5,
+        "cleaning" : 4,
+        "pissing about" : 25}
+    a = staff.values()
+    for item in a:
+        sum +=b.get(item)
+    if sum <= 80:
+        return 'kill me now'
+    elif sum > 80 and sum <100:
+        return 'i can handle this'
+    elif sum >= 100:
+        return 'party time!!'
