f64f18b2e0144b21629e16e1521859e48da70b1b
AlexandrMolyavin <117174004+AlexandrMolyavin@users.noreply.github.com>
2022-11-01 16:44:11 +0300

    Update Check if a triangle is an equable triangle!.py

--- 7_kyu/Check if a triangle is an equable triangle!.py
+++ 7_kyu/Check if a triangle is an equable triangle!.py
@@ -1,3 +1,7 @@
-def row_sum_odd_numbers(n):
-    return n**3
+def equable_triangle(a,b,c):
+    p = (a +b +c)/2
+    s = (p*(p-a)*(p-b)*(p-c))
+    s = s**0.5
+    return True if s ==p*2 else False
+    
     #your code here
