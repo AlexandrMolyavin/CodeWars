213d7996a2f550bebc836c95299e30b3b4f26d73
AlexandrMolyavin <117174004+AlexandrMolyavin@users.noreply.github.com>
2022-11-01 16:38:40 +0300

    Update Well of Ideas - Easy Version.py

--- 8_kyu/Well of Ideas - Easy Version.py
+++ 8_kyu/Well of Ideas - Easy Version.py
@@ -1,6 +1,12 @@
-def generate_range(min, max, step):
-    a = []
-    while min <= max:
-        a.append(min)
-        min += step
-    return a
+def well(x):
+    #your code here
+    i = 0
+    for item in x:
+        if item == 'good':
+            i+=1
+    if i == 0:    
+             return 'Fail!' 
+    if i == 1 or i == 2:
+                return 'Publish!' 
+    if i > 2:
+                return 'I smell a series!' 
